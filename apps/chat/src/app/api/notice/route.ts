96f9f399a9d6c0667f709be6058c2ea586fc97fc
hupengxing <hupengxing@hupengxing.com>
2023-06-09 09:26:53 +0800

    feature: 没有 notice 时去掉 404

--- apps/chat/src/app/api/notice/route.ts
+++ apps/chat/src/app/api/notice/route.ts
@@ -10,9 +10,6 @@ export async function GET(req: NextRequest) {
     return NextResponse.json(
       {
         status: ResponseStatus.Failed,
-      },
-      {
-        status: 404,
       }
     );
 
